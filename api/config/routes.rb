5def659d25e5d31b1cadfc03086333300d82f73a
Emilie Maniglier <emilie.maniglier@kinoba.fr>
2024-01-05 13:51:14 +0100

    feat: add swagger doc

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-docs'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :products
