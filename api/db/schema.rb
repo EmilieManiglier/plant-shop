8f6e5aae468424789a44d5233da9c0fdc3b350af
Emilie Maniglier <emilie.maniglier@kinoba.fr>
2024-01-05 15:02:05 +0100

    feat: add product_categories

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_05_083044) do
+ActiveRecord::Schema.define(version: 2024_01_05_134506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -24,4 +39,6 @@ ActiveRecord::Schema.define(version: 2024_01_05_083044) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
