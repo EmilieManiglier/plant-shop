0e7260a95f9d207f75ce80cf8b06eaf53a433881
Emilie Maniglier <emilie.maniglier@kinoba.fr>
2024-01-09 16:07:23 +0100

    feat(devise): add jti to users

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
+# User
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
          :rememberable,
          :validatable,
          :jwt_authenticatable,
-         jwt_revocation_strategy: JwtDenylist
+         jwt_revocation_strategy: self
 end
