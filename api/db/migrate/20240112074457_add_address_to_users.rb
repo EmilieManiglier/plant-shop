0ffdca5029186dec1fc67590cf516c6ab7104c50
Emilie Maniglier <emilie.maniglier@kinoba.fr>
2024-01-12 09:00:06 +0100

    feat: add address to users

---
+++ api/db/migrate/20240112074457_add_address_to_users.rb
@@ -0,0 +1,8 @@
+class AddAddressToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :street, :string
+    add_column :users, :city, :string
+    add_column :users, :country, :string
+    add_column :users, :zip_code, :string
+  end
+end
